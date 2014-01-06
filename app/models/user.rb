06824d69fe98a37b5a6bc04d2cafb9dd82178612
Cevaris <cevaris@gmail.com>
2014-01-05 22:12:53 -0700

    in progress, edited user to handle contact info

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,25 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  
+  has_one :contact
+
+
+  before_create :on_create
+
+  private
+  
+    def on_create
+      # Do something
+    end
+
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.for(:account_update) do |u|
+        u.permit(:first_name, :last_name, :email, :password,
+        # This is important for nested attributes
+        :phone_number, :skype, :timeoutablezone,
+        :password_confirmation, :current_password)
+      end
+    end
+
 end
