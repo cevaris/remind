b20b18670ef01b74c493bd22a08dd3cf5eb402a9
Cevaris <cevaris@gmail.com>
2014-01-05 22:18:09 -0700

    working on edit form

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,20 +1,12 @@
 class User < ActiveRecord::Base
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  
-  has_one :contact
-
-
-  before_create :on_create
 
   private
   
-    def on_create
-      # Do something
-    end
-
     def configure_permitted_parameters
       devise_parameter_sanitizer.for(:account_update) do |u|
         u.permit(:first_name, :last_name, :email, :password,
