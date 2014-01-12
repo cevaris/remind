f080cd236079a8e20ab403f57a062eee32d7ff0f
Cevaris <cevaris@gmail.com>
2014-01-12 15:28:56 -0700

    ignored CSRF for sms resposne

--- app/controllers/mailer_controller.rb
+++ app/controllers/mailer_controller.rb
@@ -1,13 +1,11 @@
 class MailerController < ApplicationController
 
+  protect_from_forgery :except => ["sms_receive"]
+
 
   def sms_receive 
     Rails.logger.info params
-    
-    message_body = params["Body"]
-    from_number = params["From"]
- 
-    Rails.logger.info "#{from_number}, #{message_body}"
+  
     
     render nothing: true, status: 200
   end
